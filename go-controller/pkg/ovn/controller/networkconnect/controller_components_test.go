ovn-kubernetes/ovn-kubernetes

Add service controller plumbing
This commit implements the plumbing to
add services watcher to network connect controller
and adds reconcileService which determines what
all CNCs need to be reconciled for service
create events and needsServiceUpdate util to
only process service create events and ignore
updates or deletes since those are taken
care of by services controller and we don't need
to do anything.

Network connect controller only needs to add
load balancer attachments to connected networks (future
commits) while delete of service leads to load balancer
deletes that removes references from the switches thanks
to OVS's garbage collection mechanism.

Signed-off-by: Surya Seetharaman <suryaseetharaman.9@gmail.com>

go-controller/pkg/ovn/controller/networkconnect/controller_components_test.go
@@ -20,6 +20,8 @@ import (
 	networkconnectv1 "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/clusternetworkconnect/v1"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/types"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/factory"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/networkmanager"
+	ovntypes "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
 )
 
@@ -484,4 +486,395 @@ func TestController_reconcileNode(t *testing.T) {
 		defer reconciledMutex.Unlock()
 		return reconciledCNCs.UnsortedList()
 	}).Should(gomega.ConsistOf("cnc1", "cnc2"))
+}
+
+// Test for serviceNeedsUpdate function
+func TestServiceNeedsUpdate(t *testing.T) {
+	tests := []struct {
+		name     string
+		oldObj   *corev1.Service
+		newObj   *corev1.Service
+		expected bool
+	}{
+		{
+			name:     "create event (oldObj nil)",
+			oldObj:   nil,
+			newObj:   &corev1.Service{},
+			expected: true,
+		},
+		{
+			name:     "delete event (newObj nil)",
+			oldObj:   &corev1.Service{},
+			newObj:   nil,
+			expected: false,
+		},
+		{
+			name:     "both nil",
+			oldObj:   nil,
+			newObj:   nil,
+			expected: false,
+		},
+		{
+			name: "update event - should not trigger",
+			oldObj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "svc1",
+					Namespace: "default",
+				},
+			},
+			newObj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "svc1",
+					Namespace: "default",
+					Labels:    map[string]string{"app": "test"},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "spec change - should not trigger",
+			oldObj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "svc1",
+					Namespace: "default",
+				},
+				Spec: corev1.ServiceSpec{
+					ClusterIP: "10.96.0.1",
+				},
+			},
+			newObj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "svc1",
+					Namespace: "default",
+				},
+				Spec: corev1.ServiceSpec{
+					ClusterIP: "10.96.0.2",
+				},
+			},
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := serviceNeedsUpdate(tt.oldObj, tt.newObj)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+// Test for mustProcessCNCForService function
+func TestMustProcessCNCForService(t *testing.T) {
+	tests := []struct {
+		name            string
+		cncCache        map[string]*networkConnectState
+		cnc             *networkconnectv1.ClusterNetworkConnect
+		svc             *corev1.Service
+		networkOwnerKey string
+		expected        bool
+	}{
+		{
+			name:     "CNC without ClusterIPServiceNetwork - should return false",
+			cncCache: map[string]*networkConnectState{},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.PodNetwork},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "default"},
+			},
+			networkOwnerKey: "layer3_1",
+			expected:        false,
+		},
+		{
+			name:     "CNC with ClusterIPServiceNetwork but not in cache - should return false",
+			cncCache: map[string]*networkConnectState{},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "default"},
+			},
+			networkOwnerKey: "layer3_1",
+			expected:        false,
+		},
+		{
+			name: "CNC in cache but network not connected - should return false",
+			cncCache: map[string]*networkConnectState{
+				"cnc1": {
+					name:              "cnc1",
+					tunnelID:          12345,
+					connectedNetworks: sets.New("layer3_2", "layer3_3"),
+				},
+			},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "default"},
+			},
+			networkOwnerKey: "layer3_1",
+			expected:        false,
+		},
+		{
+			name: "CNC in cache and network is connected - should return true",
+			cncCache: map[string]*networkConnectState{
+				"cnc1": {
+					name:              "cnc1",
+					tunnelID:          12345,
+					connectedNetworks: sets.New("layer3_1", "layer3_2"),
+				},
+			},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "default"},
+			},
+			networkOwnerKey: "layer3_1",
+			expected:        true,
+		},
+		{
+			name: "CNC with multiple connectivity types including ClusterIPServiceNetwork - should return true",
+			cncCache: map[string]*networkConnectState{
+				"cnc1": {
+					name:              "cnc1",
+					tunnelID:          12345,
+					connectedNetworks: sets.New("layer2_5"),
+				},
+			},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{
+						networkconnectv1.PodNetwork,
+						networkconnectv1.ClusterIPServiceNetwork,
+					},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "ns1"},
+			},
+			networkOwnerKey: "layer2_5",
+			expected:        true,
+		},
+		{
+			name: "Different CNC in cache - should return false",
+			cncCache: map[string]*networkConnectState{
+				"cnc2": {
+					name:              "cnc2",
+					tunnelID:          67890,
+					connectedNetworks: sets.New("layer3_1"),
+				},
+			},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "default"},
+			},
+			networkOwnerKey: "layer3_1",
+			expected:        false,
+		},
+		{
+			name: "Empty connected networks set - should return false",
+			cncCache: map[string]*networkConnectState{
+				"cnc1": {
+					name:              "cnc1",
+					tunnelID:          12345,
+					connectedNetworks: sets.New[string](),
+				},
+			},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "default"},
+			},
+			networkOwnerKey: "layer3_1",
+			expected:        false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Controller{
+				cncCache: tt.cncCache,
+			}
+			result := c.mustProcessCNCForService(tt.svc, tt.cnc, tt.networkOwnerKey)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+// TestController_reconcileService tests that reconcileService requeues matching CNCs
+func TestController_reconcileService(t *testing.T) {
+	g := gomega.NewWithT(t)
+
+	config.OVNKubernetesFeature.EnableMultiNetwork = true
+	config.OVNKubernetesFeature.EnableNetworkSegmentation = true
+	config.OVNKubernetesFeature.EnableNetworkConnect = true
+
+	// Create NetInfo for the UDN network (layer3, ID=1)
+	netInfo, err := createNetInfo(testNetwork{
+		name:         "udn1",
+		id:           1,
+		topologyType: ovntypes.Layer3Topology,
+	})
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+
+	fakeClientset := util.GetOVNClientset().GetOVNKubeControllerClientset()
+
+	// Create test CNCs - cnc1 has ClusterIPServiceNetwork and connects layer3_1
+	// cnc2 has ClusterIPServiceNetwork but connects different network
+	// cnc3 has only PodNetwork
+	cnc1 := &networkconnectv1.ClusterNetworkConnect{
+		ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+		Spec: networkconnectv1.ClusterNetworkConnectSpec{
+			Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+		},
+	}
+	cnc2 := &networkconnectv1.ClusterNetworkConnect{
+		ObjectMeta: metav1.ObjectMeta{Name: "cnc2"},
+		Spec: networkconnectv1.ClusterNetworkConnectSpec{
+			Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+		},
+	}
+	cnc3 := &networkconnectv1.ClusterNetworkConnect{
+		ObjectMeta: metav1.ObjectMeta{Name: "cnc3"},
+		Spec: networkconnectv1.ClusterNetworkConnectSpec{
+			Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.PodNetwork},
+		},
+	}
+	_, err = fakeClientset.NetworkConnectClient.K8sV1().ClusterNetworkConnects().Create(
+		context.Background(), cnc1, metav1.CreateOptions{})
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+	_, err = fakeClientset.NetworkConnectClient.K8sV1().ClusterNetworkConnects().Create(
+		context.Background(), cnc2, metav1.CreateOptions{})
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+	_, err = fakeClientset.NetworkConnectClient.K8sV1().ClusterNetworkConnects().Create(
+		context.Background(), cnc3, metav1.CreateOptions{})
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+
+	// Create test service in namespace "ns1"
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "svc1",
+			Namespace: "ns1",
+		},
+	}
+	_, err = fakeClientset.KubeClient.CoreV1().Services("ns1").Create(
+		context.Background(), svc, metav1.CreateOptions{})
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+
+	wf, err := factory.NewOVNKubeControllerWatchFactory(fakeClientset)
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+
+	err = wf.Start()
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+	defer wf.Shutdown()
+
+	// Wait for informer caches to sync
+	syncCtx, syncCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer syncCancel()
+	synced := cache.WaitForCacheSync(
+		syncCtx.Done(),
+		wf.ClusterNetworkConnectInformer().Informer().HasSynced,
+		wf.ServiceCoreInformer().Informer().HasSynced,
+	)
+	g.Expect(synced).To(gomega.BeTrue(), "informer caches should sync")
+
+	// Track reconciled CNCs
+	reconciledCNCs := sets.New[string]()
+	reconciledMutex := sync.Mutex{}
+
+	// Create FakeNetworkManager - namespace "ns1" uses UDN network
+	fakeNetworkManager := &networkmanager.FakeNetworkManager{
+		PrimaryNetworks: map[string]util.NetInfo{
+			"ns1": netInfo, // ns1 uses the UDN network (layer3_1)
+		},
+	}
+
+	// Create controller with listers and network manager
+	c := &Controller{
+		cncLister:      wf.ClusterNetworkConnectInformer().Lister(),
+		serviceLister:  wf.ServiceCoreInformer().Lister(),
+		networkManager: fakeNetworkManager,
+		cncCache: map[string]*networkConnectState{
+			"cnc1": {
+				name:              "cnc1",
+				tunnelID:          12345,
+				connectedNetworks: sets.New("layer3_1", "layer3_2"), // connects layer3_1
+			},
+			"cnc2": {
+				name:              "cnc2",
+				tunnelID:          67890,
+				connectedNetworks: sets.New("layer3_3", "layer3_4"), // doesn't connect layer3_1
+			},
+			"cnc3": {
+				name:              "cnc3",
+				tunnelID:          11111,
+				connectedNetworks: sets.New("layer3_1"), // connects layer3_1 but no ClusterIPServiceNetwork
+			},
+		},
+	}
+
+	// Create CNC controller with custom reconcile function that tracks calls
+	cncCfg := &controllerutil.ControllerConfig[networkconnectv1.ClusterNetworkConnect]{
+		RateLimiter: workqueue.DefaultTypedControllerRateLimiter[string](),
+		Informer:    wf.ClusterNetworkConnectInformer().Informer(),
+		Lister:      wf.ClusterNetworkConnectInformer().Lister().List,
+		Reconcile: func(key string) error {
+			reconciledMutex.Lock()
+			defer reconciledMutex.Unlock()
+			reconciledCNCs.Insert(key)
+			return nil
+		},
+		ObjNeedsUpdate: cncNeedsUpdate,
+		Threadiness:    1,
+	}
+	c.cncController = controllerutil.NewController("test-cnc-controller", cncCfg)
+
+	err = controllerutil.Start(c.cncController)
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+	defer controllerutil.Stop(c.cncController)
+
+	// Wait for initial sync, then clear recorded reconciliations
+	g.Eventually(func() int {
+		reconciledMutex.Lock()
+		defer reconciledMutex.Unlock()
+		return reconciledCNCs.Len()
+	}).Should(gomega.BeNumerically(">=", 3))
+	reconciledMutex.Lock()
+	reconciledCNCs = sets.New[string]()
+	reconciledMutex.Unlock()
+
+	// Call reconcileService
+	err = c.reconcileService("ns1/svc1")
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+
+	// Verify only cnc1 was requeued (has ClusterIPServiceNetwork AND connects layer3_1)
+	// cnc2 doesn't connect layer3_1, cnc3 has only PodNetwork
+	g.Eventually(func() []string {
+		reconciledMutex.Lock()
+		defer reconciledMutex.Unlock()
+		return reconciledCNCs.UnsortedList()
+	}).Should(gomega.ConsistOf("cnc1"))
 }

pperiyasamy/openshift-ovn-kubernetes

go-controller/pkg/ovn/controller/networkconnect/controller_components_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/crd/types"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/factory"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/networkmanager"
+	ovntypes "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/types"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/util"
 )
 
@@ -609,6 +610,400 @@ func TestController_syncNAD(t *testing.T) {
 	g.Expect(err).ToNot(gomega.HaveOccurred())
 
 	// Verify CNC was requeued
+	g.Eventually(func() []string {
+		reconciledMutex.Lock()
+		defer reconciledMutex.Unlock()
+		return reconciledCNCs.UnsortedList()
+	}).Should(gomega.ConsistOf("cnc1"))
+}
+
+// Test for serviceNeedsUpdate function
+func TestServiceNeedsUpdate(t *testing.T) {
+	tests := []struct {
+		name     string
+		oldObj   *corev1.Service
+		newObj   *corev1.Service
+		expected bool
+	}{
+		{
+			name:     "create event (oldObj nil)",
+			oldObj:   nil,
+			newObj:   &corev1.Service{},
+			expected: true,
+		},
+		{
+			name:     "delete event (newObj nil)",
+			oldObj:   &corev1.Service{},
+			newObj:   nil,
+			expected: false,
+		},
+		{
+			name:     "both nil",
+			oldObj:   nil,
+			newObj:   nil,
+			expected: false,
+		},
+		{
+			name: "update event - should not trigger",
+			oldObj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "svc1",
+					Namespace: "default",
+				},
+			},
+			newObj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "svc1",
+					Namespace: "default",
+					Labels:    map[string]string{"app": "test"},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "spec change - should not trigger",
+			oldObj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "svc1",
+					Namespace: "default",
+				},
+				Spec: corev1.ServiceSpec{
+					ClusterIP: "10.96.0.1",
+				},
+			},
+			newObj: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "svc1",
+					Namespace: "default",
+				},
+				Spec: corev1.ServiceSpec{
+					ClusterIP: "10.96.0.2",
+				},
+			},
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := serviceNeedsUpdate(tt.oldObj, tt.newObj)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+// Test for mustProcessCNCForService function
+func TestMustProcessCNCForService(t *testing.T) {
+	tests := []struct {
+		name            string
+		cncCache        map[string]*networkConnectState
+		cnc             *networkconnectv1.ClusterNetworkConnect
+		svc             *corev1.Service
+		networkOwnerKey string
+		expected        bool
+	}{
+		{
+			name:     "CNC without ClusterIPServiceNetwork - should return false",
+			cncCache: map[string]*networkConnectState{},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.PodNetwork},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "default"},
+			},
+			networkOwnerKey: "layer3_1",
+			expected:        false,
+		},
+		{
+			name:     "CNC with ClusterIPServiceNetwork but not in cache - should return false",
+			cncCache: map[string]*networkConnectState{},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "default"},
+			},
+			networkOwnerKey: "layer3_1",
+			expected:        false,
+		},
+		{
+			name: "CNC in cache but network not connected - should return false",
+			cncCache: map[string]*networkConnectState{
+				"cnc1": {
+					name:              "cnc1",
+					tunnelID:          12345,
+					connectedNetworks: sets.New("layer3_2", "layer3_3"),
+				},
+			},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "default"},
+			},
+			networkOwnerKey: "layer3_1",
+			expected:        false,
+		},
+		{
+			name: "CNC in cache and network is connected - should return true",
+			cncCache: map[string]*networkConnectState{
+				"cnc1": {
+					name:              "cnc1",
+					tunnelID:          12345,
+					connectedNetworks: sets.New("layer3_1", "layer3_2"),
+				},
+			},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "default"},
+			},
+			networkOwnerKey: "layer3_1",
+			expected:        true,
+		},
+		{
+			name: "CNC with multiple connectivity types including ClusterIPServiceNetwork - should return true",
+			cncCache: map[string]*networkConnectState{
+				"cnc1": {
+					name:              "cnc1",
+					tunnelID:          12345,
+					connectedNetworks: sets.New("layer2_5"),
+				},
+			},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{
+						networkconnectv1.PodNetwork,
+						networkconnectv1.ClusterIPServiceNetwork,
+					},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "ns1"},
+			},
+			networkOwnerKey: "layer2_5",
+			expected:        true,
+		},
+		{
+			name: "Different CNC in cache - should return false",
+			cncCache: map[string]*networkConnectState{
+				"cnc2": {
+					name:              "cnc2",
+					tunnelID:          67890,
+					connectedNetworks: sets.New("layer3_1"),
+				},
+			},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "default"},
+			},
+			networkOwnerKey: "layer3_1",
+			expected:        false,
+		},
+		{
+			name: "Empty connected networks set - should return false",
+			cncCache: map[string]*networkConnectState{
+				"cnc1": {
+					name:              "cnc1",
+					tunnelID:          12345,
+					connectedNetworks: sets.New[string](),
+				},
+			},
+			cnc: &networkconnectv1.ClusterNetworkConnect{
+				ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+				Spec: networkconnectv1.ClusterNetworkConnectSpec{
+					Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+				},
+			},
+			svc: &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{Name: "svc1", Namespace: "default"},
+			},
+			networkOwnerKey: "layer3_1",
+			expected:        false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Controller{
+				cncCache: tt.cncCache,
+			}
+			result := c.mustProcessCNCForService(tt.svc, tt.cnc, tt.networkOwnerKey)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+// TestController_reconcileService tests that reconcileService requeues matching CNCs
+func TestController_reconcileService(t *testing.T) {
+	g := gomega.NewWithT(t)
+
+	err := config.PrepareTestConfig()
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+
+	config.OVNKubernetesFeature.EnableMultiNetwork = true
+	config.OVNKubernetesFeature.EnableNetworkSegmentation = true
+	config.OVNKubernetesFeature.EnableNetworkConnect = true
+
+	// Create NetInfo for the UDN network (layer3, ID=1)
+	netInfo, err := createNetInfo(testNetwork{
+		name:         "udn1",
+		id:           1,
+		topologyType: ovntypes.Layer3Topology,
+	})
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+
+	fakeClientset := util.GetOVNClientset().GetOVNKubeControllerClientset()
+
+	// Create test CNCs - cnc1 has ClusterIPServiceNetwork and connects layer3_1
+	// cnc2 has ClusterIPServiceNetwork but connects different network
+	// cnc3 has only PodNetwork
+	cnc1 := &networkconnectv1.ClusterNetworkConnect{
+		ObjectMeta: metav1.ObjectMeta{Name: "cnc1"},
+		Spec: networkconnectv1.ClusterNetworkConnectSpec{
+			Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+		},
+	}
+	cnc2 := &networkconnectv1.ClusterNetworkConnect{
+		ObjectMeta: metav1.ObjectMeta{Name: "cnc2"},
+		Spec: networkconnectv1.ClusterNetworkConnectSpec{
+			Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.ClusterIPServiceNetwork},
+		},
+	}
+	cnc3 := &networkconnectv1.ClusterNetworkConnect{
+		ObjectMeta: metav1.ObjectMeta{Name: "cnc3"},
+		Spec: networkconnectv1.ClusterNetworkConnectSpec{
+			Connectivity: []networkconnectv1.ConnectivityType{networkconnectv1.PodNetwork},
+		},
+	}
+	_, err = fakeClientset.NetworkConnectClient.K8sV1().ClusterNetworkConnects().Create(
+		context.Background(), cnc1, metav1.CreateOptions{})
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+	_, err = fakeClientset.NetworkConnectClient.K8sV1().ClusterNetworkConnects().Create(
+		context.Background(), cnc2, metav1.CreateOptions{})
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+	_, err = fakeClientset.NetworkConnectClient.K8sV1().ClusterNetworkConnects().Create(
+		context.Background(), cnc3, metav1.CreateOptions{})
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+
+	// Create test service in namespace "ns1"
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "svc1",
+			Namespace: "ns1",
+		},
+	}
+	_, err = fakeClientset.KubeClient.CoreV1().Services("ns1").Create(
+		context.Background(), svc, metav1.CreateOptions{})
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+
+	wf, err := factory.NewOVNKubeControllerWatchFactory(fakeClientset)
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+
+	err = wf.Start()
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+	defer wf.Shutdown()
+
+	// Wait for informer caches to sync
+	syncCtx, syncCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer syncCancel()
+	synced := cache.WaitForCacheSync(
+		syncCtx.Done(),
+		wf.ClusterNetworkConnectInformer().Informer().HasSynced,
+		wf.ServiceCoreInformer().Informer().HasSynced,
+	)
+	g.Expect(synced).To(gomega.BeTrue(), "informer caches should sync")
+
+	// Track reconciled CNCs
+	reconciledCNCs := sets.New[string]()
+	reconciledMutex := sync.Mutex{}
+
+	// Create FakeNetworkManager - namespace "ns1" uses UDN network
+	fakeNetworkManager := &networkmanager.FakeNetworkManager{
+		PrimaryNetworks: map[string]util.NetInfo{
+			"ns1": netInfo, // ns1 uses the UDN network (layer3_1)
+		},
+	}
+
+	// Create controller with listers and network manager
+	c := &Controller{
+		cncLister:      wf.ClusterNetworkConnectInformer().Lister(),
+		serviceLister:  wf.ServiceCoreInformer().Lister(),
+		networkManager: fakeNetworkManager,
+		cncCache: map[string]*networkConnectState{
+			"cnc1": {
+				name:              "cnc1",
+				tunnelID:          12345,
+				connectedNetworks: sets.New("layer3_1", "layer3_2"), // connects layer3_1
+			},
+			"cnc2": {
+				name:              "cnc2",
+				tunnelID:          67890,
+				connectedNetworks: sets.New("layer3_3", "layer3_4"), // doesn't connect layer3_1
+			},
+			"cnc3": {
+				name:              "cnc3",
+				tunnelID:          11111,
+				connectedNetworks: sets.New("layer3_1"), // connects layer3_1 but no ClusterIPServiceNetwork
+			},
+		},
+	}
+
+	// Create CNC controller with custom reconcile function that tracks calls
+	cncCfg := &controllerutil.ControllerConfig[networkconnectv1.ClusterNetworkConnect]{
+		RateLimiter: workqueue.DefaultTypedControllerRateLimiter[string](),
+		Informer:    wf.ClusterNetworkConnectInformer().Informer(),
+		Lister:      wf.ClusterNetworkConnectInformer().Lister().List,
+		Reconcile: func(key string) error {
+			reconciledMutex.Lock()
+			defer reconciledMutex.Unlock()
+			reconciledCNCs.Insert(key)
+			return nil
+		},
+		ObjNeedsUpdate: cncNeedsUpdate,
+		Threadiness:    1,
+	}
+	c.cncController = controllerutil.NewController("test-cnc-controller", cncCfg)
+
+	err = controllerutil.Start(c.cncController)
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+	defer controllerutil.Stop(c.cncController)
+
+	// Wait for initial sync, then clear recorded reconciliations
+	g.Eventually(func() int {
+		reconciledMutex.Lock()
+		defer reconciledMutex.Unlock()
+		return reconciledCNCs.Len()
+	}).Should(gomega.BeNumerically(">=", 3))
+	reconciledMutex.Lock()
+	reconciledCNCs = sets.New[string]()
+	reconciledMutex.Unlock()
+
+	// Call reconcileService
+	err = c.reconcileService("ns1/svc1")
+	g.Expect(err).ToNot(gomega.HaveOccurred())
+
+	// Verify only cnc1 was requeued (has ClusterIPServiceNetwork AND connects layer3_1)
+	// cnc2 doesn't connect layer3_1, cnc3 has only PodNetwork
 	g.Eventually(func() []string {
 		reconciledMutex.Lock()
 		defer reconciledMutex.Unlock()