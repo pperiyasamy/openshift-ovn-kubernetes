ovn-kubernetes/ovn-kubernetes

Switch to a separate address set manager
It follow the logic and interfaces of pod_selector_address_set
from the ovn package, but uses a single level-driven controller
instead of per-address-set retry frameworks and is separated from
the BNC.
We can drop the previous address set format cleanup, because
it's been present since release-1.0.
2 cleanup unit tests are dropped, because the cleanup logic has
been simplified. Previously we had to stop all handlers and
now we only need to delete address set.

Signed-off-by: Nadia Pinaeva <n.m.pinaeva@gmail.com>

go-controller/pkg/ovn/base_network_controller.go
@@ -38,6 +38,7 @@ import (
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/networkmanager"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/observability"
 	addressset "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/address_set"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/addresssetmanager"
 	nqoscontroller "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/controller/network_qos"
 	lsm "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/logical_switch_manager"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/routeimport"
@@ -153,6 +154,7 @@ type BaseNetworkController struct {
 	sharedNetpolPortGroups *syncmap.SyncMap[*defaultDenyPortGroups]
 
 	podSelectorAddressSets *syncmap.SyncMap[*PodSelectorAddressSet]
+	addressSetManager      *addresssetmanager.AddressSetManager
 
 	// stopChan per controller
 	stopChan chan struct{}

go-controller/pkg/ovn/default_network_controller.go
@@ -25,6 +25,7 @@ import (
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/networkmanager"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/observability"
 	addressset "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/address_set"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/addresssetmanager"
 	anpcontroller "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/controller/admin_network_policy"
 	apbroutecontroller "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/controller/apbroute"
 	efcontroller "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/controller/egressfirewall"
@@ -238,6 +239,9 @@ func newDefaultNetworkControllerCommon(
 		svcController:              svcController,
 		gatewayTopologyFactory:     topology.NewGatewayTopologyFactory(cnci.nbClient),
 	}
+	oc.addressSetManager = addresssetmanager.NewAddressSetManager(oc.watchFactory.PodCoreInformer(),
+		oc.watchFactory.NamespaceInformer(), oc.nbClient, oc.addressSetFactory,
+		oc.controllerName, oc.GetNetInfo(), oc.getNetworkNameForNADKeyFunc())
 	// Allocate IPs for logical router port "GwRouterToJoinSwitchPrefix + OVNClusterRouter". This should always
 	// allocate the first IPs in the join switch subnets.
 	gwLRPIfAddrs, err := oc.getOVNClusterRouterPortToJoinSwitchIfAddrs()
@@ -355,6 +359,9 @@ func (oc *DefaultNetworkController) Stop() {
 	if oc.networkConnectController != nil {
 		oc.networkConnectController.Stop()
 	}
+	if oc.addressSetManager != nil {
+		oc.addressSetManager.Stop()
+	}
 
 	close(oc.stopChan)
 	oc.cancelableCtx.Cancel()
@@ -443,6 +450,10 @@ func (oc *DefaultNetworkController) run(_ context.Context) error {
 	}
 
 	if err := WithSyncDurationMetric("pod", oc.WatchPods); err != nil {
+		return err
+	}
+
+	if err := WithSyncDurationMetric("pod IP address sets", oc.addressSetManager.Start); err != nil {
 		return err
 	}
 

go-controller/pkg/ovn/layer2_user_defined_network_controller.go
@@ -28,6 +28,7 @@ import (
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/nbdb"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/networkmanager"
 	addressset "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/address_set"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/addresssetmanager"
 	svccontroller "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/controller/services"
 	lsm "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/logical_switch_manager"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/routeimport"
@@ -398,6 +399,9 @@ func NewLayer2UserDefinedNetworkController(
 		eIPController:          eIPController,
 		remoteNodesNoRouter:    sync.Map{},
 	}
+	oc.addressSetManager = addresssetmanager.NewAddressSetManager(oc.watchFactory.PodCoreInformer(),
+		oc.watchFactory.NamespaceInformer(), oc.nbClient, oc.addressSetFactory,
+		oc.controllerName, oc.GetNetInfo(), oc.getNetworkNameForNADKeyFunc())
 	if oc.IsPrimaryNetwork() && oc.eIPController != nil {
 		oc.onLogicalPortCacheAdd = func(pod *corev1.Pod, _ string) {
 			oc.eIPController.addEgressIPPodRetry(pod, "logical port cache update")

go-controller/pkg/ovn/layer3_user_defined_network_controller.go
@@ -23,6 +23,7 @@ import (
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/nbdb"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/networkmanager"
 	addressset "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/address_set"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/addresssetmanager"
 	svccontroller "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/controller/services"
 	lsm "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/logical_switch_manager"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/routeimport"
@@ -385,6 +386,9 @@ func NewLayer3UserDefinedNetworkController(
 		gatewayManagers:             sync.Map{},
 		eIPController:               eIPController,
 	}
+	oc.addressSetManager = addresssetmanager.NewAddressSetManager(oc.watchFactory.PodCoreInformer(),
+		oc.watchFactory.NamespaceInformer(), oc.nbClient, oc.addressSetFactory,
+		oc.controllerName, oc.GetNetInfo(), oc.getNetworkNameForNADKeyFunc())
 	if oc.IsPrimaryNetwork() && oc.eIPController != nil {
 		oc.onLogicalPortCacheAdd = func(pod *corev1.Pod, _ string) {
 			oc.eIPController.addEgressIPPodRetry(pod, "logical port cache update")
@@ -511,6 +515,9 @@ func (oc *Layer3UserDefinedNetworkController) Stop() {
 	if oc.routeImportManager != nil {
 		oc.routeImportManager.ForgetNetwork(oc.GetNetworkName())
 	}
+	if oc.addressSetManager != nil {
+		oc.addressSetManager.Stop()
+	}
 }
 
 // Cleanup cleans up logical entities for the given network, called from net-attach-def routine
@@ -612,6 +619,10 @@ func (oc *Layer3UserDefinedNetworkController) run() error {
 	}
 
 	if err := oc.WatchPods(); err != nil {
+		return err
+	}
+
+	if err := oc.addressSetManager.Start(); err != nil {
 		return err
 	}
 

go-controller/pkg/ovn/localnet_user_defined_network_controller.go
@@ -19,6 +19,7 @@ import (
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/nbdb"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/networkmanager"
 	addressset "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/address_set"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/addresssetmanager"
 	lsm "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/logical_switch_manager"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/persistentips"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/retry"
@@ -223,6 +224,9 @@ func NewLocalnetUserDefinedNetworkController(
 			},
 		},
 	}
+	oc.addressSetManager = addresssetmanager.NewAddressSetManager(oc.watchFactory.PodCoreInformer(),
+		oc.watchFactory.NamespaceInformer(), oc.nbClient, oc.addressSetFactory,
+		oc.controllerName, oc.GetNetInfo(), oc.getNetworkNameForNADKeyFunc())
 
 	if oc.allocatesPodAnnotation() {
 		var claimsReconciler persistentips.PersistentAllocations

pperiyasamy/openshift-ovn-kubernetes

go-controller/pkg/ovn/base_network_controller.go
@@ -38,6 +38,7 @@ import (
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/networkmanager"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/observability"
 	addressset "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/address_set"
+	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/addresssetmanager"
 	nqoscontroller "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/controller/network_qos"
 	lsm "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/logical_switch_manager"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/routeimport"
@@ -153,6 +154,7 @@ type BaseNetworkController struct {
 	sharedNetpolPortGroups *syncmap.SyncMap[*defaultDenyPortGroups]
 
 	podSelectorAddressSets *syncmap.SyncMap[*PodSelectorAddressSet]
+	addressSetManager      *addresssetmanager.AddressSetManager
 
 	// stopChan per controller
 	stopChan chan struct{}

go-controller/pkg/ovn/default_network_controller.go
@@ -25,6 +25,7 @@ import (
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/networkmanager"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/observability"
 	addressset "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/address_set"
+	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/addresssetmanager"
 	anpcontroller "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/controller/admin_network_policy"
 	apbroutecontroller "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/controller/apbroute"
 	efcontroller "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/controller/egressfirewall"
@@ -238,6 +239,9 @@ func newDefaultNetworkControllerCommon(
 		svcController:              svcController,
 		gatewayTopologyFactory:     topology.NewGatewayTopologyFactory(cnci.nbClient),
 	}
+	oc.addressSetManager = addresssetmanager.NewAddressSetManager(oc.watchFactory.PodCoreInformer(),
+		oc.watchFactory.NamespaceInformer(), oc.nbClient, oc.addressSetFactory,
+		oc.controllerName, oc.GetNetInfo(), oc.getNetworkNameForNADKeyFunc())
 	// Allocate IPs for logical router port "GwRouterToJoinSwitchPrefix + OVNClusterRouter". This should always
 	// allocate the first IPs in the join switch subnets.
 	gwLRPIfAddrs, err := oc.getOVNClusterRouterPortToJoinSwitchIfAddrs()
@@ -356,6 +360,9 @@ func (oc *DefaultNetworkController) Stop() {
 	if oc.networkConnectController != nil {
 		oc.networkConnectController.Stop()
 	}
+	if oc.addressSetManager != nil {
+		oc.addressSetManager.Stop()
+	}
 
 	close(oc.stopChan)
 	oc.cancelableCtx.Cancel()
@@ -444,6 +451,10 @@ func (oc *DefaultNetworkController) run(_ context.Context) error {
 	}
 
 	if err := WithSyncDurationMetric("pod", oc.WatchPods); err != nil {
+		return err
+	}
+
+	if err := WithSyncDurationMetric("pod IP address sets", oc.addressSetManager.Start); err != nil {
 		return err
 	}
 

go-controller/pkg/ovn/layer2_user_defined_network_controller.go
@@ -28,6 +28,7 @@ import (
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/nbdb"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/networkmanager"
 	addressset "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/address_set"
+	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/addresssetmanager"
 	svccontroller "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/controller/services"
 	lsm "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/logical_switch_manager"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/routeimport"
@@ -398,6 +399,9 @@ func NewLayer2UserDefinedNetworkController(
 		eIPController:          eIPController,
 		remoteNodesNoRouter:    sync.Map{},
 	}
+	oc.addressSetManager = addresssetmanager.NewAddressSetManager(oc.watchFactory.PodCoreInformer(),
+		oc.watchFactory.NamespaceInformer(), oc.nbClient, oc.addressSetFactory,
+		oc.controllerName, oc.GetNetInfo(), oc.getNetworkNameForNADKeyFunc())
 	if oc.IsPrimaryNetwork() && oc.eIPController != nil {
 		oc.onLogicalPortCacheAdd = func(pod *corev1.Pod, _ string) {
 			oc.eIPController.addEgressIPPodRetry(pod, "logical port cache update")

go-controller/pkg/ovn/layer3_user_defined_network_controller.go
@@ -23,6 +23,7 @@ import (
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/nbdb"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/networkmanager"
 	addressset "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/address_set"
+	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/addresssetmanager"
 	svccontroller "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/controller/services"
 	lsm "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/logical_switch_manager"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/routeimport"
@@ -385,6 +386,9 @@ func NewLayer3UserDefinedNetworkController(
 		gatewayManagers:             sync.Map{},
 		eIPController:               eIPController,
 	}
+	oc.addressSetManager = addresssetmanager.NewAddressSetManager(oc.watchFactory.PodCoreInformer(),
+		oc.watchFactory.NamespaceInformer(), oc.nbClient, oc.addressSetFactory,
+		oc.controllerName, oc.GetNetInfo(), oc.getNetworkNameForNADKeyFunc())
 	if oc.IsPrimaryNetwork() && oc.eIPController != nil {
 		oc.onLogicalPortCacheAdd = func(pod *corev1.Pod, _ string) {
 			oc.eIPController.addEgressIPPodRetry(pod, "logical port cache update")
@@ -511,6 +515,9 @@ func (oc *Layer3UserDefinedNetworkController) Stop() {
 	}
 	if oc.routeImportManager != nil {
 		oc.routeImportManager.ForgetNetwork(oc.GetNetworkName())
+	}
+	if oc.addressSetManager != nil {
+		oc.addressSetManager.Stop()
 	}
 }
 
@@ -646,6 +653,10 @@ func (oc *Layer3UserDefinedNetworkController) run() error {
 	}
 
 	if err := oc.WatchPods(); err != nil {
+		return err
+	}
+
+	if err := oc.addressSetManager.Start(); err != nil {
 		return err
 	}
 

go-controller/pkg/ovn/localnet_user_defined_network_controller.go
@@ -19,6 +19,7 @@ import (
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/nbdb"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/networkmanager"
 	addressset "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/address_set"
+	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/addresssetmanager"
 	lsm "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/ovn/logical_switch_manager"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/persistentips"
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/retry"
@@ -223,6 +224,9 @@ func NewLocalnetUserDefinedNetworkController(
 			},
 		},
 	}
+	oc.addressSetManager = addresssetmanager.NewAddressSetManager(oc.watchFactory.PodCoreInformer(),
+		oc.watchFactory.NamespaceInformer(), oc.nbClient, oc.addressSetFactory,
+		oc.controllerName, oc.GetNetInfo(), oc.getNetworkNameForNADKeyFunc())
 
 	if oc.allocatesPodAnnotation() {
 		var claimsReconciler persistentips.PersistentAllocations