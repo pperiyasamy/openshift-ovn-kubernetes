ovn-kubernetes/ovn-kubernetes

Add VTEP informers/clients for EVPN support
Signed-off-by: Patryk Diak <pdiak@redhat.com>

go-controller/pkg/factory/mocks/NodeWatchFactory.go
@@ -25,6 +25,8 @@ import (
 	userdefinednetworkv1 "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/userdefinednetwork/v1/apis/informers/externalversions/userdefinednetwork/v1"
 
 	v1 "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/adminpolicybasedroute/v1/apis/informers/externalversions/adminpolicybasedroute/v1"
+
+	vtepv1 "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/vtep/v1/apis/informers/externalversions/vtep/v1"
 )
 
 // NodeWatchFactory is an autogenerated mock type for the NodeWatchFactory type
@@ -799,6 +801,26 @@ func (_m *NodeWatchFactory) UserDefinedNetworkInformer() userdefinednetworkv1.Us
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(userdefinednetworkv1.UserDefinedNetworkInformer)
+		}
+	}
+
+	return r0
+}
+
+// VTEPInformer provides a mock function with no fields
+func (_m *NodeWatchFactory) VTEPInformer() vtepv1.VTEPInformer {
+	ret := _m.Called()
+
+	if len(ret) == 0 {
+		panic("no return value specified for VTEPInformer")
+	}
+
+	var r0 vtepv1.VTEPInformer
+	if rf, ok := ret.Get(0).(func() vtepv1.VTEPInformer); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(vtepv1.VTEPInformer)
 		}
 	}
 

go-controller/pkg/factory/types.go
@@ -13,6 +13,7 @@ import (
 	egressipinformer "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressip/v1/apis/informers/externalversions/egressip/v1"
 	routeadvertisementsinformer "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/routeadvertisements/v1/apis/informers/externalversions/routeadvertisements/v1"
 	userdefinednetworkinformer "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/userdefinednetwork/v1/apis/informers/externalversions/userdefinednetwork/v1"
+	vtepinformer "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/vtep/v1/apis/informers/externalversions/vtep/v1"
 )
 
 // ObjectCacheInterface represents the exported methods for getting
@@ -65,6 +66,7 @@ type NodeWatchFactory interface {
 	UserDefinedNetworkInformer() userdefinednetworkinformer.UserDefinedNetworkInformer
 	ClusterUserDefinedNetworkInformer() userdefinednetworkinformer.ClusterUserDefinedNetworkInformer
 	RouteAdvertisementsInformer() routeadvertisementsinformer.RouteAdvertisementsInformer
+	VTEPInformer() vtepinformer.VTEPInformer
 
 	GetPods(namespace string) ([]*corev1.Pod, error)
 	GetPod(namespace, name string) (*corev1.Pod, error)

pperiyasamy/openshift-ovn-kubernetes

go-controller/pkg/factory/mocks/NodeWatchFactory.go
@@ -25,6 +25,8 @@ import (
 	userdefinednetworkv1 "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/crd/userdefinednetwork/v1/apis/informers/externalversions/userdefinednetwork/v1"
 
 	v1 "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/crd/adminpolicybasedroute/v1/apis/informers/externalversions/adminpolicybasedroute/v1"
+
+	vtepv1 "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/crd/vtep/v1/apis/informers/externalversions/vtep/v1"
 )
 
 // NodeWatchFactory is an autogenerated mock type for the NodeWatchFactory type
@@ -799,6 +801,26 @@ func (_m *NodeWatchFactory) UserDefinedNetworkInformer() userdefinednetworkv1.Us
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(userdefinednetworkv1.UserDefinedNetworkInformer)
+		}
+	}
+
+	return r0
+}
+
+// VTEPInformer provides a mock function with no fields
+func (_m *NodeWatchFactory) VTEPInformer() vtepv1.VTEPInformer {
+	ret := _m.Called()
+
+	if len(ret) == 0 {
+		panic("no return value specified for VTEPInformer")
+	}
+
+	var r0 vtepv1.VTEPInformer
+	if rf, ok := ret.Get(0).(func() vtepv1.VTEPInformer); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(vtepv1.VTEPInformer)
 		}
 	}
 

go-controller/pkg/factory/types.go
@@ -13,6 +13,7 @@ import (
 	egressipinformer "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/crd/egressip/v1/apis/informers/externalversions/egressip/v1"
 	routeadvertisementsinformer "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/crd/routeadvertisements/v1/apis/informers/externalversions/routeadvertisements/v1"
 	userdefinednetworkinformer "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/crd/userdefinednetwork/v1/apis/informers/externalversions/userdefinednetwork/v1"
+	vtepinformer "github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/crd/vtep/v1/apis/informers/externalversions/vtep/v1"
 )
 
 // ObjectCacheInterface represents the exported methods for getting
@@ -65,6 +66,7 @@ type NodeWatchFactory interface {
 	UserDefinedNetworkInformer() userdefinednetworkinformer.UserDefinedNetworkInformer
 	ClusterUserDefinedNetworkInformer() userdefinednetworkinformer.ClusterUserDefinedNetworkInformer
 	RouteAdvertisementsInformer() routeadvertisementsinformer.RouteAdvertisementsInformer
+	VTEPInformer() vtepinformer.VTEPInformer
 
 	GetPods(namespace string) ([]*corev1.Pod, error)
 	GetPod(namespace, name string) (*corev1.Pod, error)