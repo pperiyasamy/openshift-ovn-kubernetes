pperiyasamy/ovn-kubernetes

e2e: fix primary IP allocator for multi-subnet clusters
The primary IP allocator panics when nodes span multiple subnets.
A single-subnet allocator cannot produce valid EgressIPs for nodes
across different subnets.

Refactor to per-subnet allocation: callers pass eligible node subnet
CIDR(s) from `k8s.ovn.org/node-primary-ifaddr`, and the allocator lazily
creates a /24 (IPv4) or /120 (IPv6) allocator per subnet, producing
IPs within the correct subnet.

Co-Authored-By: Claude Opus 4.6 <noreply@anthropic.com>
Signed-off-by: Periyasamy Palanisamy <pepalani@redhat.com>

test/e2e/ipalloc/primaryipalloc.go
@@ -4,21 +4,38 @@
 package ipalloc
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"net"
+	"sync"
+
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/util"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
-	"net"
-	"sync"
 )
 
-// primaryIPAllocator attempts to allocate an IP in the same subnet as a nodes primary network
+// Reserved range for E2E test IPs to avoid conflicts with node IPs
+const (
+	testIPv4Start = 200 // Start from .200 to avoid typical node IPs (.1-.199)
+	testIPv4End   = 254 // Last usable before .255 broadcast
+	testIPv6Start = 200 // Same for IPv6 (0xC8)
+	testIPv6End   = 255 // Last byte of reserved range (0xFF)
+)
+
+// allocState holds per-subnet allocator state
+type allocState struct {
+	alloc *ipAllocator
+	maxIP net.IP // upper bound of reserved range (e.g. .254)
+}
+
+// primaryIPAllocator attempts to allocate an IP in the same subnet as a node's primary network.
+// It maintains per-subnet allocators to support clusters where nodes span multiple subnets.
 type primaryIPAllocator struct {
 	mu         *sync.Mutex
-	v4         *ipAllocator
-	v6         *ipAllocator
+	v4Allocs   map[string]*allocState // keyed by subnet prefix (last byte zeroed)
+	v6Allocs   map[string]*allocState
 	nodeClient v1.NodeInterface
 }
 
@@ -31,18 +48,23 @@ func InitPrimaryIPAllocator(nodeClient v1.NodeInterface) error {
 	return err
 }
 
-func NewPrimaryIPv4() (net.IP, error) {
-	return pia.AllocateNextV4()
+func NewPrimaryIPv4(subnets ...string) (net.IP, error) {
+	return pia.AllocateNextV4(subnets...)
 }
 
-func NewPrimaryIPv6() (net.IP, error) {
-	return pia.AllocateNextV6()
+func NewPrimaryIPv6(subnets ...string) (net.IP, error) {
+	return pia.AllocateNextV6(subnets...)
 }
 
-// newPrimaryIPAllocator gets a Nodes primary interfaces network info, increments the 2 octet and checks if the IP is still
-// within the subnet of all the K8 nodes.
+// newPrimaryIPAllocator initializes a primaryIPAllocator with empty per-subnet maps.
+// Subnet allocators are created lazily when AllocateNextV4/V6 is called with specific subnet CIDRs.
 func newPrimaryIPAllocator(nodeClient v1.NodeInterface) (*primaryIPAllocator, error) {
-	ipa := &primaryIPAllocator{mu: &sync.Mutex{}, nodeClient: nodeClient}
+	ipa := &primaryIPAllocator{
+		mu:         &sync.Mutex{},
+		nodeClient: nodeClient,
+		v4Allocs:   make(map[string]*allocState),
+		v6Allocs:   make(map[string]*allocState),
+	}
 	nodes, err := nodeClient.List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		return ipa, fmt.Errorf("failed to get a list of node(s): %v", err)
@@ -50,140 +72,149 @@ func newPrimaryIPAllocator(nodeClient v1.NodeInterface) (*primaryIPAllocator, er
 	if len(nodes.Items) == 0 {
 		return ipa, fmt.Errorf("expected at least one node but found zero")
 	}
-	// FIXME: the approach taken here to find the first node IP+mask and then to increment the second last octet wont work in
-	// all scenarios (node with /24). We should generate an EgressIP compatible with a Node providers primary network and then take care its unique globally.
-
-	// The approach here is to grab initial starting IP from first node found, increment the second last octet.
-	// Approach taken here won't work for Nodes handed /24 subnets.
-	nodePrimaryIPs, err := util.ParseNodePrimaryIfAddr(&nodes.Items[0])
-	if err != nil {
-		return ipa, fmt.Errorf("failed to parse node primary interface address from Node object: %v", err)
-	}
-	if nodePrimaryIPs.V4.IP != nil {
-		// should be ok with /16 and /64 node primary provider subnets
-		// TODO; fixme; what about /24 subnet Nodes like GCP
-		nodePrimaryIPs.V4.IP[len(nodePrimaryIPs.V4.IP)-2]++
-		ipa.v4 = newIPAllocator(&net.IPNet{IP: nodePrimaryIPs.V4.IP, Mask: nodePrimaryIPs.V4.Net.Mask})
-	}
-	if nodePrimaryIPs.V6.IP != nil {
-		nodePrimaryIPs.V6.IP[len(nodePrimaryIPs.V6.IP)-2]++
-		ipa.v6 = newIPAllocator(&net.IPNet{IP: nodePrimaryIPs.V6.IP, Mask: nodePrimaryIPs.V6.Net.Mask})
-	}
-	// verify the new starting base IP is within all Nodes subnets
-	if nodePrimaryIPs.V4.IP != nil {
-		ipNets, err := getNodePrimaryProviderIPs(nodes.Items, false)
-		if err != nil {
-			return ipa, err
-		}
-		nextIP, err := ipa.v4.AllocateNextIP()
-		if err != nil {
-			return ipa, err
-		}
-		if !isIPWithinAllSubnets(ipNets, nextIP) {
-			return ipa, fmt.Errorf("IP %s is not within all Node subnets", nextIP)
-		}
-	}
-	if nodePrimaryIPs.V6.IP != nil {
-		ipNets, err := getNodePrimaryProviderIPs(nodes.Items, true)
-		if err != nil {
-			return ipa, err
-		}
-		nextIP, err := ipa.v6.AllocateNextIP()
-		if err != nil {
-			return ipa, err
-		}
-		if !isIPWithinAllSubnets(ipNets, nextIP) {
-			return ipa, fmt.Errorf("IP %s is not within all Node subnets", nextIP)
-		}
-	}
-
 	return ipa, nil
 }
 
-func getNodePrimaryProviderIPs(nodes []corev1.Node, isIPv6 bool) ([]*net.IPNet, error) {
-	ipNets := make([]*net.IPNet, 0, len(nodes))
-	for _, node := range nodes {
-		nodePrimaryIPs, err := util.ParseNodePrimaryIfAddr(&node)
+// subnetKey returns a map key that groups IPs sharing the same first N-1 bytes.
+// The last byte is zeroed so that e.g. 10.0.0.4 and 10.0.0.5 produce the same key.
+func subnetKey(ip net.IP) string {
+	ip16 := make(net.IP, 16)
+	copy(ip16, ip.To16())
+	ip16[15] = 0
+	return string(ip16)
+}
+
+// getOrCreateV4 returns or lazily creates a per-subnet IPv4 allocator.
+// Uses a /24 mask internally so the allocator produces IPs in the .200-.254 range
+// regardless of the annotation's actual mask (which may be /17, /32, etc.).
+func (p *primaryIPAllocator) getOrCreateV4(ip net.IP) *allocState {
+	key := subnetKey(ip)
+	if state, ok := p.v4Allocs[key]; ok {
+		return state
+	}
+	ip4 := ip.To4()
+	startIP := make(net.IP, 4)
+	copy(startIP, ip4)
+	startIP[3] = testIPv4Start - 1 // AllocateNextIP increments before returning
+	maxIP := make(net.IP, 4)
+	copy(maxIP, ip4)
+	maxIP[3] = testIPv4End
+	state := &allocState{
+		alloc: newIPAllocator(&net.IPNet{IP: startIP, Mask: net.CIDRMask(24, 32)}),
+		maxIP: maxIP.To16(),
+	}
+	p.v4Allocs[key] = state
+	return state
+}
+
+// getOrCreateV6 returns or lazily creates a per-subnet IPv6 allocator.
+// Uses a /120 mask internally so the allocator produces IPs in the ::c8-::ff range.
+func (p *primaryIPAllocator) getOrCreateV6(ip net.IP) *allocState {
+	key := subnetKey(ip)
+	if state, ok := p.v6Allocs[key]; ok {
+		return state
+	}
+	ip16 := ip.To16()
+	startIP := make(net.IP, 16)
+	copy(startIP, ip16)
+	startIP[15] = testIPv6Start - 1
+	maxIP := make(net.IP, 16)
+	copy(maxIP, ip16)
+	maxIP[15] = testIPv6End
+	state := &allocState{
+		alloc: newIPAllocator(&net.IPNet{IP: startIP, Mask: net.CIDRMask(120, 128)}),
+		maxIP: maxIP,
+	}
+	p.v6Allocs[key] = state
+	return state
+}
+
+// AllocateNextV4 allocates the next available IPv4 from the reserved range (.200-.254)
+// within one of the provided subnet CIDRs. Each subnet string should be a CIDR like "10.0.0.4/17"
+// derived from the node's k8s.ovn.org/node-primary-ifaddr annotation.
+func (p *primaryIPAllocator) AllocateNextV4(subnets ...string) (net.IP, error) {
+	if len(subnets) == 0 {
+		return nil, fmt.Errorf("at least one subnet must be provided")
+	}
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	var lastErr error
+	for _, subnet := range subnets {
+		ip, _, err := net.ParseCIDR(subnet)
 		if err != nil {
-			return nil, fmt.Errorf("failed to parse node primary interface address from Node %s object: %v", node.Name, err)
+			lastErr = err
+			continue
 		}
-		var mask net.IPMask
-		var ip net.IP
-
-		if isIPv6 {
-			ip = nodePrimaryIPs.V6.IP
-			mask = nodePrimaryIPs.V6.Net.Mask
-		} else {
-			ip = nodePrimaryIPs.V4.IP
-			mask = nodePrimaryIPs.V4.Net.Mask
+		ip4 := ip.To4()
+		if ip4 == nil {
+			continue
 		}
-		if len(ip) == 0 || len(mask) == 0 {
-			return nil, fmt.Errorf("failed to find Node %s primary Node IP and/or mask", node.Name)
+		state := p.getOrCreateV4(ip4)
+		allocated, err := allocateIP(p.nodeClient, state.alloc.AllocateNextIP, state.maxIP)
+		if err != nil {
+			lastErr = err
+			continue // try next subnet
 		}
-		ipNets = append(ipNets, &net.IPNet{IP: ip, Mask: mask})
+		return allocated, nil
 	}
-	return ipNets, nil
+	return nil, fmt.Errorf("failed to allocate IPv4 from any of the provided subnets: %v", lastErr)
 }
 
-func isIPWithinAllSubnets(ipNets []*net.IPNet, ip net.IP) bool {
-	if len(ipNets) == 0 {
-		return false
+// AllocateNextV6 allocates the next available IPv6 from the reserved range (::c8-::ff)
+// within one of the provided subnet CIDRs.
+func (p *primaryIPAllocator) AllocateNextV6(subnets ...string) (net.IP, error) {
+	if len(subnets) == 0 {
+		return nil, fmt.Errorf("at least one subnet must be provided")
 	}
-	for _, ipNet := range ipNets {
-		if !ipNet.Contains(ip) {
-			return false
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	var lastErr error
+	for _, subnet := range subnets {
+		ip, _, err := net.ParseCIDR(subnet)
+		if err != nil {
+			lastErr = err
+			continue
 		}
+		if ip.To4() != nil {
+			continue // skip IPv4
+		}
+		state := p.getOrCreateV6(ip.To16())
+		allocated, err := allocateIP(p.nodeClient, state.alloc.AllocateNextIP, state.maxIP)
+		if err != nil {
+			lastErr = err
+			continue // try next subnet
+		}
+		return allocated, nil
 	}
-	return true
+	return nil, fmt.Errorf("failed to allocate IPv6 from any of the provided subnets: %v", lastErr)
 }
 
-func (pia *primaryIPAllocator) IncrementAndGetNextV4(times int) (net.IP, error) {
+func (pia *primaryIPAllocator) IncrementAndGetNextV4(times int, subnets ...string) (net.IP, error) {
 	var err error
 	for i := 0; i < times; i++ {
-		if _, err = pia.AllocateNextV4(); err != nil {
+		if _, err = pia.AllocateNextV4(subnets...); err != nil {
 			return nil, err
 		}
 	}
-	return pia.AllocateNextV4()
+	return pia.AllocateNextV4(subnets...)
 }
 
-func (pia *primaryIPAllocator) AllocateNextV4() (net.IP, error) {
-	if pia.v4 == nil {
-		return nil, fmt.Errorf("IPv4 is not enable ")
-	}
-	if pia.v4.net == nil {
-		return nil, fmt.Errorf("IPv4 is not enabled but Allocation request was called")
-	}
-	pia.mu.Lock()
-	defer pia.mu.Unlock()
-	return allocateIP(pia.nodeClient, pia.v4.AllocateNextIP)
-}
-
-func (pia *primaryIPAllocator) IncrementAndGetNextV6(times int) (net.IP, error) {
+func (pia *primaryIPAllocator) IncrementAndGetNextV6(times int, subnets ...string) (net.IP, error) {
 	var err error
 	for i := 0; i < times; i++ {
-		if _, err = pia.AllocateNextV6(); err != nil {
+		if _, err = pia.AllocateNextV6(subnets...); err != nil {
 			return nil, err
 		}
 	}
-	return pia.AllocateNextV6()
-}
-
-func (pia primaryIPAllocator) AllocateNextV6() (net.IP, error) {
-	if pia.v6 == nil {
-		return nil, fmt.Errorf("IPv6 is not enabled but Allocation request was called")
-	}
-	if pia.v6.net == nil {
-		return nil, fmt.Errorf("ipv6 network is not set")
-	}
-	pia.mu.Lock()
-	defer pia.mu.Unlock()
-	return allocateIP(pia.nodeClient, pia.v6.AllocateNextIP)
+	return pia.AllocateNextV6(subnets...)
 }
 
 type allocNextFn func() (net.IP, error)
 
-func allocateIP(nodeClient v1.NodeInterface, allocateFn allocNextFn) (net.IP, error) {
+// allocateIP allocates the next available IP from the reserved range (.200-.254 for IPv4,
+// ::c8-::ff for IPv6) that doesn't conflict with existing node IPs. maxIP is the upper
+// bound (inclusive) of the reserved range; any candidate beyond it triggers an exhaustion error.
+func allocateIP(nodeClient v1.NodeInterface, allocateFn allocNextFn, maxIP net.IP) (net.IP, error) {
 	nodeList, err := nodeClient.List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to list nodes: %v", err)
@@ -191,11 +222,15 @@ func allocateIP(nodeClient v1.NodeInterface, allocateFn allocNextFn) (net.IP, er
 	for {
 		nextIP, err := allocateFn()
 		if err != nil {
-			return nil, fmt.Errorf("failed to allocated next IP address: %v", err)
+			return nil, fmt.Errorf("failed to allocate next IP address (reserved test range may be exhausted): %v", err)
 		}
-		firstOctet := nextIP[len(nextIP)-1]
-		// skip 0 and 1
-		if firstOctet == 0 || firstOctet == 1 {
+		if bytes.Compare(nextIP, maxIP) > 0 {
+			return nil, fmt.Errorf("reserved IP range exhausted: next candidate %s exceeds maximum %s", net.IP(nextIP), net.IP(maxIP))
+		}
+		lastOctet := nextIP[len(nextIP)-1]
+		// Skip reserved addresses (.0 is network, .1 typically gateway)
+		// This shouldn't happen since we start from .200, but check anyway
+		if lastOctet == 0 || lastOctet == 1 {
 			continue
 		}
 		isConflict, err := isConflictWithExistingHostIPs(nodeList.Items, nextIP)
@@ -205,6 +240,7 @@ func allocateIP(nodeClient v1.NodeInterface, allocateFn allocNextFn) (net.IP, er
 		if !isConflict {
 			return nextIP, nil
 		}
+		// IP conflicts with a node, try next one
 	}
 }
 

pperiyasamy/openshift-ovn-kubernetes

(cherry picked from commit 07280499957d708b50e07584637c71c20fdaa1bd)

test/e2e/ipalloc/primaryipalloc.go
@@ -1,24 +1,43 @@
 // SPDX-FileCopyrightText: Copyright The OVN-Kubernetes Contributors
 // SPDX-License-Identifier: Apache-2.0
 
+// Package ipalloc allocates individual IPs from a node's primary network
+// for E2E tests (e.g. EgressIP, EgressService, Service). This is distinct
+// from the allocators package which allocates UDN, BGP peering, IPVRF and
+// VTEP subnets and VIDs/VNIs across parallel tests.
+// TODO: add IP release functionality to allow reuse of allocated IPs.
 package ipalloc
 
 import (
 	"context"
 	"fmt"
+	"net"
+	"sync"
+
 	"github.com/ovn-kubernetes/ovn-kubernetes/go-controller/pkg/util"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
-	"net"
-	"sync"
 )
 
-// primaryIPAllocator attempts to allocate an IP in the same subnet as a nodes primary network
+// Reserved range for E2E test IPs to avoid conflicts with node IPs
+const (
+	testIPv4Start = 200 // Start from .200 to avoid typical node IPs (.1-.199)
+	testIPv4End   = 254 // Last usable before .255 broadcast
+	testIPv6Start = 200 // Same for IPv6 (0xC8)
+	testIPv6End   = 255 // Last byte of reserved range (0xFF)
+)
+
+// primaryIPAllocator is a cluster-wide IP allocator for E2E tests. Callers pass a node's
+// primary subnet CIDR when requesting an IP, and the allocator returns an unused IP from
+// the reserved range (.200-.254 for IPv4, ::c8-::ff for IPv6) within that subnet.
+// A separate allocator is maintained per subnet so that nodes on different subnets receive
+// IPs from their own subnet's range.
+// The nodeClient is used to list existing node IPs and avoid conflicts.
 type primaryIPAllocator struct {
 	mu         *sync.Mutex
-	v4         *ipAllocator
-	v6         *ipAllocator
+	v4Allocs   map[string]*ipAllocator // keyed by subnet prefix (last byte zeroed)
+	v6Allocs   map[string]*ipAllocator
 	nodeClient v1.NodeInterface
 }
 
@@ -31,18 +50,23 @@ func InitPrimaryIPAllocator(nodeClient v1.NodeInterface) error {
 	return err
 }
 
-func NewPrimaryIPv4() (net.IP, error) {
-	return pia.AllocateNextV4()
+func NewPrimaryIPv4(subnet string) (net.IP, error) {
+	return pia.AllocateNextV4(subnet)
 }
 
-func NewPrimaryIPv6() (net.IP, error) {
-	return pia.AllocateNextV6()
+func NewPrimaryIPv6(subnet string) (net.IP, error) {
+	return pia.AllocateNextV6(subnet)
 }
 
-// newPrimaryIPAllocator gets a Nodes primary interfaces network info, increments the 2 octet and checks if the IP is still
-// within the subnet of all the K8 nodes.
+// newPrimaryIPAllocator initializes a primaryIPAllocator with empty per-subnet maps.
+// Subnet allocators are created lazily when AllocateNextV4/V6 is called with specific subnet CIDRs.
 func newPrimaryIPAllocator(nodeClient v1.NodeInterface) (*primaryIPAllocator, error) {
-	ipa := &primaryIPAllocator{mu: &sync.Mutex{}, nodeClient: nodeClient}
+	ipa := &primaryIPAllocator{
+		mu:         &sync.Mutex{},
+		nodeClient: nodeClient,
+		v4Allocs:   make(map[string]*ipAllocator),
+		v6Allocs:   make(map[string]*ipAllocator),
+	}
 	nodes, err := nodeClient.List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		return ipa, fmt.Errorf("failed to get a list of node(s): %v", err)
@@ -50,140 +74,113 @@ func newPrimaryIPAllocator(nodeClient v1.NodeInterface) (*primaryIPAllocator, er
 	if len(nodes.Items) == 0 {
 		return ipa, fmt.Errorf("expected at least one node but found zero")
 	}
-	// FIXME: the approach taken here to find the first node IP+mask and then to increment the second last octet wont work in
-	// all scenarios (node with /24). We should generate an EgressIP compatible with a Node providers primary network and then take care its unique globally.
-
-	// The approach here is to grab initial starting IP from first node found, increment the second last octet.
-	// Approach taken here won't work for Nodes handed /24 subnets.
-	nodePrimaryIPs, err := util.ParseNodePrimaryIfAddr(&nodes.Items[0])
-	if err != nil {
-		return ipa, fmt.Errorf("failed to parse node primary interface address from Node object: %v", err)
-	}
-	if nodePrimaryIPs.V4.IP != nil {
-		// should be ok with /16 and /64 node primary provider subnets
-		// TODO; fixme; what about /24 subnet Nodes like GCP
-		nodePrimaryIPs.V4.IP[len(nodePrimaryIPs.V4.IP)-2]++
-		ipa.v4 = newIPAllocator(&net.IPNet{IP: nodePrimaryIPs.V4.IP, Mask: nodePrimaryIPs.V4.Net.Mask})
-	}
-	if nodePrimaryIPs.V6.IP != nil {
-		nodePrimaryIPs.V6.IP[len(nodePrimaryIPs.V6.IP)-2]++
-		ipa.v6 = newIPAllocator(&net.IPNet{IP: nodePrimaryIPs.V6.IP, Mask: nodePrimaryIPs.V6.Net.Mask})
-	}
-	// verify the new starting base IP is within all Nodes subnets
-	if nodePrimaryIPs.V4.IP != nil {
-		ipNets, err := getNodePrimaryProviderIPs(nodes.Items, false)
-		if err != nil {
-			return ipa, err
-		}
-		nextIP, err := ipa.v4.AllocateNextIP()
-		if err != nil {
-			return ipa, err
-		}
-		if !isIPWithinAllSubnets(ipNets, nextIP) {
-			return ipa, fmt.Errorf("IP %s is not within all Node subnets", nextIP)
-		}
-	}
-	if nodePrimaryIPs.V6.IP != nil {
-		ipNets, err := getNodePrimaryProviderIPs(nodes.Items, true)
-		if err != nil {
-			return ipa, err
-		}
-		nextIP, err := ipa.v6.AllocateNextIP()
-		if err != nil {
-			return ipa, err
-		}
-		if !isIPWithinAllSubnets(ipNets, nextIP) {
-			return ipa, fmt.Errorf("IP %s is not within all Node subnets", nextIP)
-		}
-	}
-
 	return ipa, nil
 }
 
-func getNodePrimaryProviderIPs(nodes []corev1.Node, isIPv6 bool) ([]*net.IPNet, error) {
-	ipNets := make([]*net.IPNet, 0, len(nodes))
-	for _, node := range nodes {
-		nodePrimaryIPs, err := util.ParseNodePrimaryIfAddr(&node)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse node primary interface address from Node %s object: %v", node.Name, err)
-		}
-		var mask net.IPMask
-		var ip net.IP
-
-		if isIPv6 {
-			ip = nodePrimaryIPs.V6.IP
-			mask = nodePrimaryIPs.V6.Net.Mask
-		} else {
-			ip = nodePrimaryIPs.V4.IP
-			mask = nodePrimaryIPs.V4.Net.Mask
-		}
-		if len(ip) == 0 || len(mask) == 0 {
-			return nil, fmt.Errorf("failed to find Node %s primary Node IP and/or mask", node.Name)
-		}
-		ipNets = append(ipNets, &net.IPNet{IP: ip, Mask: mask})
-	}
-	return ipNets, nil
+// subnetKey groups IPs by their /24 (IPv4) or /120 (IPv6) prefix. Two nodes in the
+// same broad subnet (e.g. /17) but different /24s get separate allocators, which is
+// fine since allocateIP rechecks against all node IPs to avoid conflicts.
+func subnetKey(ip net.IP) string {
+	return string(ip.To16().Mask(net.CIDRMask(120, 128)))
 }
 
-func isIPWithinAllSubnets(ipNets []*net.IPNet, ip net.IP) bool {
-	if len(ipNets) == 0 {
-		return false
+// getOrCreateV4 returns or lazily creates a per-subnet IPv4 allocator.
+// Uses a fixed /24 mask internally so the allocator produces IPs in the .200-.254 range
+// regardless of the annotation's actual mask. This is intentional because some cloud
+// providers use narrow masks (e.g. /32) in the node-primary-ifaddr annotation even though
+// the underlying network is broader and the allocated IPs are routable.
+func (p *primaryIPAllocator) getOrCreateV4(ip net.IP) *ipAllocator {
+	key := subnetKey(ip)
+	if alloc, ok := p.v4Allocs[key]; ok {
+		return alloc
 	}
-	for _, ipNet := range ipNets {
-		if !ipNet.Contains(ip) {
-			return false
-		}
-	}
-	return true
+	ip4 := ip.To4()
+	startIP := make(net.IP, 4)
+	copy(startIP, ip4)
+	startIP[3] = testIPv4Start - 1 // AllocateNextIP increments before returning
+	alloc := newIPAllocator(&net.IPNet{IP: startIP, Mask: net.CIDRMask(24, 32)})
+	p.v4Allocs[key] = alloc
+	return alloc
 }
 
-func (pia *primaryIPAllocator) IncrementAndGetNextV4(times int) (net.IP, error) {
+// getOrCreateV6 returns or lazily creates a per-subnet IPv6 allocator.
+// Uses a fixed /120 mask internally so the allocator produces IPs in the ::c8-::ff range.
+// See getOrCreateV4 for why the annotation's actual mask is intentionally ignored.
+func (p *primaryIPAllocator) getOrCreateV6(ip net.IP) *ipAllocator {
+	key := subnetKey(ip)
+	if alloc, ok := p.v6Allocs[key]; ok {
+		return alloc
+	}
+	ip16 := ip.To16()
+	startIP := make(net.IP, 16)
+	copy(startIP, ip16)
+	startIP[15] = testIPv6Start - 1
+	alloc := newIPAllocator(&net.IPNet{IP: startIP, Mask: net.CIDRMask(120, 128)})
+	p.v6Allocs[key] = alloc
+	return alloc
+}
+
+// AllocateNextV4 allocates the next available IPv4 from the reserved range (.200-.254)
+// within the provided subnet CIDR. The subnet string should be a CIDR like "10.0.0.0/17"
+// derived from the node's k8s.ovn.org/node-primary-ifaddr annotation.
+func (p *primaryIPAllocator) AllocateNextV4(subnet string) (net.IP, error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	ip, _, err := net.ParseCIDR(subnet)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse subnet %q: %v", subnet, err)
+	}
+	ip4 := ip.To4()
+	if ip4 == nil {
+		return nil, fmt.Errorf("subnet %q is not an IPv4 CIDR", subnet)
+	}
+	alloc := p.getOrCreateV4(ip4)
+	return allocateIP(p.nodeClient, alloc.AllocateNextIP, testIPv4Start, testIPv4End)
+}
+
+// AllocateNextV6 allocates the next available IPv6 from the reserved range (::c8-::ff)
+// within the provided subnet CIDR.
+func (p *primaryIPAllocator) AllocateNextV6(subnet string) (net.IP, error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	ip, _, err := net.ParseCIDR(subnet)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse subnet %q: %v", subnet, err)
+	}
+	if ip.To4() != nil {
+		return nil, fmt.Errorf("subnet %q is not an IPv6 CIDR", subnet)
+	}
+	alloc := p.getOrCreateV6(ip.To16())
+	return allocateIP(p.nodeClient, alloc.AllocateNextIP, testIPv6Start, testIPv6End)
+}
+
+func (pia *primaryIPAllocator) IncrementAndGetNextV4(times int, subnet string) (net.IP, error) {
 	var err error
 	for i := 0; i < times; i++ {
-		if _, err = pia.AllocateNextV4(); err != nil {
+		if _, err = pia.AllocateNextV4(subnet); err != nil {
 			return nil, err
 		}
 	}
-	return pia.AllocateNextV4()
+	return pia.AllocateNextV4(subnet)
 }
 
-func (pia *primaryIPAllocator) AllocateNextV4() (net.IP, error) {
-	if pia.v4 == nil {
-		return nil, fmt.Errorf("IPv4 is not enable ")
-	}
-	if pia.v4.net == nil {
-		return nil, fmt.Errorf("IPv4 is not enabled but Allocation request was called")
-	}
-	pia.mu.Lock()
-	defer pia.mu.Unlock()
-	return allocateIP(pia.nodeClient, pia.v4.AllocateNextIP)
-}
-
-func (pia *primaryIPAllocator) IncrementAndGetNextV6(times int) (net.IP, error) {
+func (pia *primaryIPAllocator) IncrementAndGetNextV6(times int, subnet string) (net.IP, error) {
 	var err error
 	for i := 0; i < times; i++ {
-		if _, err = pia.AllocateNextV6(); err != nil {
+		if _, err = pia.AllocateNextV6(subnet); err != nil {
 			return nil, err
 		}
 	}
-	return pia.AllocateNextV6()
-}
-
-func (pia primaryIPAllocator) AllocateNextV6() (net.IP, error) {
-	if pia.v6 == nil {
-		return nil, fmt.Errorf("IPv6 is not enabled but Allocation request was called")
-	}
-	if pia.v6.net == nil {
-		return nil, fmt.Errorf("ipv6 network is not set")
-	}
-	pia.mu.Lock()
-	defer pia.mu.Unlock()
-	return allocateIP(pia.nodeClient, pia.v6.AllocateNextIP)
+	return pia.AllocateNextV6(subnet)
 }
 
 type allocNextFn func() (net.IP, error)
 
-func allocateIP(nodeClient v1.NodeInterface, allocateFn allocNextFn) (net.IP, error) {
+// allocateIP allocates the next available IP from the reserved range (.200-.254 for IPv4,
+// ::c8-::ff for IPv6) that doesn't conflict with existing node IPs. The last byte of each
+// candidate must fall within [startLastByte, endLastByte]; any candidate outside this range
+// triggers an exhaustion error.
+func allocateIP(nodeClient v1.NodeInterface, allocateFn allocNextFn, startLastByte, endLastByte byte) (net.IP, error) {
 	nodeList, err := nodeClient.List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to list nodes: %v", err)
@@ -191,12 +188,11 @@ func allocateIP(nodeClient v1.NodeInterface, allocateFn allocNextFn) (net.IP, er
 	for {
 		nextIP, err := allocateFn()
 		if err != nil {
-			return nil, fmt.Errorf("failed to allocated next IP address: %v", err)
+			return nil, fmt.Errorf("failed to allocate next IP address (reserved test range may be exhausted): %v", err)
 		}
-		firstOctet := nextIP[len(nextIP)-1]
-		// skip 0 and 1
-		if firstOctet == 0 || firstOctet == 1 {
-			continue
+		lastOctet := nextIP[len(nextIP)-1]
+		if lastOctet < startLastByte || lastOctet > endLastByte {
+			return nil, fmt.Errorf("reserved IP range exhausted: next candidate %s is outside range [%d, %d]", net.IP(nextIP), startLastByte, endLastByte)
 		}
 		isConflict, err := isConflictWithExistingHostIPs(nodeList.Items, nextIP)
 		if err != nil {
@@ -205,6 +201,7 @@ func allocateIP(nodeClient v1.NodeInterface, allocateFn allocNextFn) (net.IP, er
 		if !isConflict {
 			return nextIP, nil
 		}
+		// IP conflicts with a node, try next one
 	}
 }
 