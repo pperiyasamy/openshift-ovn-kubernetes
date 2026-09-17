openshift/ovn-kubernetes

OTE: skip EgressIP host-net-pod E2Es on bastion platforms
Signed-off-by: Periyasamy Palanisamy <pepalani@redhat.com>

openshift/cmd/ovn-kubernetes-tests-ext/main.go
@@ -79,6 +79,11 @@ func shouldIncludeTest(spec *extensiontests.ExtensionTestSpec) bool {
 	if strings.Contains(spec.Name, "secondary-host-eip") && !ocpInfra.HasSecondaryHostEIPSupport() {
 		return false
 	}
+	// EgressIP host-networked pods tests require opening ports (30000 to 32767) on cluster nodes,
+	// which is not yet configured on bastion-based platforms.
+	if strings.Contains(spec.Name, "Should validate the egress IP SNAT functionality against host-networked pods") && ocpInfra.IsBastionBasedPlatform() {
+		return false
+	}
 
 	// FUP: not having to detect the environment, and just be able to
 	// run what we want through the definition of the appropriate test

openshift/test/infraprovider/openshift.go
@@ -143,6 +143,16 @@ func (o *OpenshiftInfraProvider) HasPlatformInfra() bool {
 	return o.clusterInfra != nil
 }
 
+// IsBastionBasedPlatform returns true for cloud platforms (AWS, GCP, Azure)
+// where external containers run on a bastion host with host networking.
+func (o *OpenshiftInfraProvider) IsBastionBasedPlatform() bool {
+	if o.clusterInfra == nil {
+		return false
+	}
+	_, isBM := o.clusterInfra.(*baremetalInfra)
+	return !isBM
+}
+
 // CheckForEVPN checks all EVPN prerequisites
 func (o *OpenshiftInfraProvider) CheckForEVPN() bool {
 	if o.operNetwork == nil {

pperiyasamy/openshift-ovn-kubernetes

openshift/cmd/ovn-kubernetes-tests-ext/main.go
@@ -79,6 +79,11 @@ func shouldIncludeTest(spec *extensiontests.ExtensionTestSpec) bool {
 	if strings.Contains(spec.Name, "secondary-host-eip") && !ocpInfra.HasSecondaryHostEIPSupport() {
 		return false
 	}
+	// EgressIP host-networked pods tests require opening ports (30000 to 32767) on cluster nodes,
+	// which is not yet configured on bastion-based platforms.
+	if strings.Contains(spec.Name, "Should validate the egress IP SNAT functionality against host-networked pods") && ocpInfra.IsCloudPlatform() {
+		return false
+	}
 
 	// FUP: not having to detect the environment, and just be able to
 	// run what we want through the definition of the appropriate test

openshift/test/infraprovider/openshift.go
@@ -143,6 +143,16 @@ func (o *OpenshiftInfraProvider) HasPlatformInfra() bool {
 	return o.clusterInfra != nil
 }
 
+// IsCloudPlatform returns true for cloud platforms (AWS, GCP, Azure)
+// where external containers run on a bastion host with host networking.
+func (o *OpenshiftInfraProvider) IsCloudPlatform() bool {
+	if o.clusterInfra == nil {
+		return false
+	}
+	_, isBM := o.clusterInfra.(*baremetalInfra)
+	return !isBM
+}
+
 // CheckForEVPN checks all EVPN prerequisites
 func (o *OpenshiftInfraProvider) CheckForEVPN() bool {
 	if o.operNetwork == nil {